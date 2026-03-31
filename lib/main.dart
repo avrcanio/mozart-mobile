0cac461c7bb256b904562be8576d02a0087cc59e
avrcanio <avrcanus@gmail.com>
2026-04-01 00:41:51 +0200

    Initialize locale data before app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
 
 import 'src/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Intl.defaultLocale = 'hr_HR';
+  await initializeDateFormatting('hr_HR');
   runApp(const MozartMobileApp());
 }
